df6c5ffae598fec88ce284930aa07b57b2707343
Mateusz Finek <mateusz_finek@epam.com>
2016-06-10 10:58:51 +0200

    Mvn tomcat7:run repair

--- Customer Shop/src/main/java/com/epam/benchshop/controllers/HomeController.java
+++ Customer Shop/src/main/java/com/epam/benchshop/controllers/HomeController.java
@@ -2,13 +2,15 @@ package com.epam.benchshop.controllers;
 
 import java.io.IOException;
 
-import javax.servlet.http.HttpServletResponse;
+
 
 import com.epam.benchshop.domain.message.MessageSupplier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 class HomeController {
